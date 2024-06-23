86face59cd3e7459f658163d42396bf5f065e14e
Ghislaine Laios <laios54@pm.me>
2024-06-23 16:31:51 +0800

    Done!

---
+++ client/scripts/generate-types.ts
@@ -0,0 +1,28 @@
+import { spawn } from 'child_process';
+import appRootPath from 'app-root-path';
+
+function main() {
+  const CLIENT_DIR = appRootPath.resolve('client');
+
+  // TODO: Add unix script
+  // TODO: Add loop
+  spawn(
+    '"./node_modules/grpc-tools/bin/protoc.exe"',
+    [
+      `--plugin=protoc-gen-ts_proto=".\\node_modules\\.bin\\protoc-gen-ts_proto.cmd"`,
+      `--ts_proto_out=".\\compiled_protos"`,
+      `--ts_proto_opt="outputServices=nice-grpc,outputServices=generic-definitions,useExactTypes=false,esModuleInterop=true,importSuffix=.js"`,
+      `--proto_path="..\\protos"`,
+      `..\\protos\\hello_world.proto`,
+    ],
+    {
+      stdio: 'inherit',
+      shell: true,
+      cwd: CLIENT_DIR,
+    },
+  );
+}
+
+console.info("----\x1b[32mSCRIPT START\x1b[0m----")
+
+main();
--- client/src/app.ts
+++ client/src/app.ts
@@ -7,29 +7,24 @@ import {
   credentials,
   loadPackageDefinition,
 } from '@grpc/grpc-js';
+import { createChannel, createClient } from 'nice-grpc';
+import {
+  HelloWorldClient,
+  HelloWorldDefinition,
+} from '../compiled_protos/hello_world.js';
 dotenv.config();
 
 export const sum = (a: number, b: number): number => {
   return a + b;
 };
 
-const HELLO_WORLD_PROTO_PATH = appRootPath.resolve('protos/hello_world.proto');
 const SERVER_ADDR = 'localhost:10000';
 
 async function main() {
-  const definition = loadSync(HELLO_WORLD_PROTO_PATH);
-  const hello_proto = loadPackageDefinition(definition);
-  const hello_package = hello_proto.hello_world as GrpcObject;
-  const hello_client =
-    new (hello_package.HelloWorld as ServiceClientConstructor)(
-      SERVER_ADDR,
-      credentials.createInsecure(),
-    );
-  // console.log(hello_client.helloWorld);
-  
-  hello_client.helloWorld();
-
-  
+  const channel = createChannel(SERVER_ADDR);
+  const client: HelloWorldClient = createClient(HelloWorldDefinition, channel);
+  const resp = await client.helloWorld({ helloString: 'hi' });
+  console.log(resp);
 }
 
 main();
