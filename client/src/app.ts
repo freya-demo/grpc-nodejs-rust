0edc80f6ce88b81d025c4ab4d49d2bca348bd33f
Ghislaine Laios <laios54@pm.me>
2024-06-23 14:30:42 +0800

    Update

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,11 +1,35 @@
-/**
- * The following lines intialize dotenv,
- * so that env vars from the .env file are present in process.env
- */
+import appRootPath from 'app-root-path';
+import { loadSync } from '@grpc/proto-loader';
 import * as dotenv from 'dotenv';
+import {
+  GrpcObject,
+  ServiceClientConstructor,
+  credentials,
+  loadPackageDefinition,
+} from '@grpc/grpc-js';
 dotenv.config();
 
 export const sum = (a: number, b: number): number => {
   return a + b;
 };
 
+const HELLO_WORLD_PROTO_PATH = appRootPath.resolve('protos/hello_world.proto');
+const SERVER_ADDR = 'localhost:10000';
+
+async function main() {
+  const definition = loadSync(HELLO_WORLD_PROTO_PATH);
+  const hello_proto = loadPackageDefinition(definition);
+  const hello_package = hello_proto.hello_world as GrpcObject;
+  const hello_client =
+    new (hello_package.HelloWorld as ServiceClientConstructor)(
+      SERVER_ADDR,
+      credentials.createInsecure(),
+    );
+  // console.log(hello_client.helloWorld);
+  
+  hello_client.helloWorld();
+
+  
+}
+
+main();
